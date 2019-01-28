1d6c49f7aa4fcbb68e9f8ed2dfae7272808161a6
Erik Zerpa <zerpaerik@gmail.com>
2019-01-28 16:31:50 -0400

    cccc

--- resources/views/reportes/comollego/search.blade.php
+++ resources/views/reportes/comollego/search.blade.php
@@ -1,15 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-</br>
+
+<body>
 <div class="row">
 	<div class="col-xs-12">
 		<div class="box">
 			<div class="box-header">
 				<div class="box-name">
-					<i class="fa fa-users"></i>
-					<span><strong>Nùmero de Atenciones</strong></span>
+					<i class="fa fa-linux"></i>
+					<span>Reportes/ Número de Atenciones</span>
+
 				</div>
+
+
 				<div class="box-icons">
 					<a class="collapse-link">
 						<i class="fa fa-chevron-up"></i>
@@ -17,20 +21,49 @@
 					<a class="expand-link">
 						<i class="fa fa-expand"></i>
 					</a>
+					<a class="close-link">
+						<i class="fa fa-times"></i>
+					</a>
 				</div>
+
 				<div class="no-move"></div>
+				
 			</div>
+			{!! Form::open(['method' => 'get', 'route' => ['comollego.search']]) !!}
+
+			<div class="row">
+				<div class="col-md-2">
+					{!! Form::label('inicio', 'Fecha Inicio', ['class' => 'control-label']) !!}
+					{!! Form::date('inicio', old('inicio'), ['id'=>'inicio','class' => 'form-control', 'placeholder' => '', 'required' => '']) !!}
+					<p class="help-block"></p>
+					@if($errors->has('inicio'))
+					<p class="help-block">
+						{{ $errors->first('inicio') }}
+					</p>
+					@endif
+				</div>
+				<div class="col-md-2">
+					{!! Form::label('final', 'Fecha Fin', ['class' => 'control-label']) !!}
+					{!! Form::date('final', old('final'), ['id'=>'final','class' => 'form-control', 'placeholder' => '', 'required' => '']) !!}
+					<p class="help-block"></p>
+					@if($errors->has('final'))
+					<p class="help-block">
+						{{ $errors->first('final') }}
+					</p>
+					@endif
+				</div>
+				<div class="col-md-2">
+					{!! Form::submit(trans('Buscar'), array('class' => 'btn btn-info')) !!}
+					{!! Form::close() !!}
+
+				</div>
+			</div>	
+
+			
+
 			<div class="box-content no-padding">
-				<table class="table table-bordered table-striped table-hover table-heading table-datatable" id="datatable-1">
-					<form action="/comollego-search" method="get">
-						<label for="">Inicio</label>
-						<input type="date" name="inicio" value="{{ Carbon\Carbon::now()->toDateString()}}" style="line-height: 20px">
-						<label for="">Final</label>
-						<input type="date" name="final" value="{{ Carbon\Carbon::now()->toDateString()}}" style="line-height: 20px">
-						<label for=""></label>
-						<input type="submit" value="Buscar" class="btn btn-primary" style="margin-left: 30px;">
-					</form>
-				<thead>
+				<table class="table table-bordered table-striped table-hover table-heading table-datatable" id="datatable-3">
+					<thead>
 						<tr>
 							<th>Vallas</th>
 							<th>Carteles</th>
@@ -45,8 +78,7 @@
 						</tr>
 					</thead>
 					<tbody>
-
-							<tr>
+                       <tr>
 								<td>{{$vallas->cantidad}}</td>
 								<td>{{$carteles->cantidad}}</td>
 								<td>{{$pacientes->cantidad}}</td>
@@ -61,17 +93,42 @@
 							</tr>
 					</tbody>
 					
-					</form>
+
 				</table>
 			</div>
-	
 		</div>
 	</div>
 </div>
-@if(isset($created))
-	<div class="alert alert-success" role="alert">
-	  A simple success alert—check it out!
-	</div>
-@endif
 
+</body>
+
+
+
+<script src="{{url('/tema/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{url('/tema/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+
+
+
+
+<script type="text/javascript">
+// Run Datables plugin and create 3 variants of settings
+function AllTables(){
+	TestTable1();
+	TestTable2();
+	TestTable3();
+	LoadSelect2Script(MakeSelect2);
+}
+function MakeSelect2(){
+	$('select').select2();
+	$('.dataTables_filter').each(function(){
+		$(this).find('label input[type=text]').attr('placeholder', 'Search');
+	});
+}
+$(document).ready(function() {
+	// Load Datatables and run plugin on tables 
+	LoadDataTablesScripts(AllTables);
+	// Add Drag-n-Drop feature
+	WinMove();
+});
+</script>
 @endsection
