93392ef324cca21490b79672328ab0b5646c617c
Erik Zerpa <zerpaerik@gmail.com>
2019-02-06 19:22:22 -0400

    ccccc

--- resources/views/layouts/partials/sidenav.blade.php
+++ resources/views/layouts/partials/sidenav.blade.php
@@ -725,6 +725,34 @@
       </li>              
     </ul>
   </li>
+    <li class="dropdown">
+    <a href="#" class="dropdown-toggle">
+      <i class="fa fa-copy"></i>
+      <span class="hidden-xs">Control de Sesiones</span>
+    </a>
+    <ul class="dropdown-menu">
+      <li>
+        <a href="{{route('resultados.index')}}"><i class="fa fa-list-alt"></i> Registrar Sesión</a>
+      </li>
+      <li>
+        <a href="{{route('resultadosguardados.index')}}"><i class="fa fa-search"></i> Consultar Sesiones</a>
+      </li>
+    </ul>
+  </li>
+  <li class="dropdown">
+    <a href="#" class="dropdown-toggle">
+      <i class="fa fa-copy"></i>
+      <span class="hidden-xs"> Control de Evaluaciones</span>
+    </a>
+    <ul class="dropdown-menu">
+      <li>
+        <a href="{{route('prenatal.create')}}"><i class="fa fa-list-alt"></i> Registrar Evaluaciòn</a>
+      </li>
+      <li>
+        <a href="{{route('prenatal.index')}}"><i class="fa fa-search"></i> Buscar Evaluaciòn</a>
+      </li>
+    </ul>
+  </li>
   <li class="dropdown">
     <a href="#" class="dropdown-toggle">
       <i class="fa fa-unsorted"></i>
