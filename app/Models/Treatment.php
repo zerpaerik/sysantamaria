b6dde6e0573e77845b4ec561a51c44c9b26fd59b
Erik Zerpa <zerpaerik@gmail.com>
2019-02-08 14:20:42 -0400

    cc

--- app/Models/Treatment.php
+++ app/Models/Treatment.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Treatment extends Model
 {
-    protected $guarded = [];
+
+      protected $fillable = [
+    	'ficha_eval','eva_eval','frecuencia_eval','exac_eval','inicio_eval','inicio_tiempo_eval','dolor_eval','retraccion_eval','parestecia_eval','hiperalgesia_eval','hiperalgesia_zona_eval','limitacion_eval','localizacion_eval','irradiacion_eval','irradiacion_zona_eval','observaciones_eval','diagnostico_eval','chc_trat','cf_trat','tiempo_trat','frecuencia_ultrasonido_trat','intensidad_ultrasonido_trat','ciclo_ultrasonido_trat','tiempo_ultrasonido_trat','dolor_laser_trat','tenosinovitis_laser_trat','esguince_laser_trat','tension_laser_trat','rusa_corriente_trat','interferencial_corriente_trat','alto_corriente_trat','tens_corriente_trat','estiramiento_trat','klapp_metodo_trat','william_metodo_trat','wilson_metodo_trat','fnp_metodo_trat','codman_metodo_trat','burguer_metodo_trat','kaltenbron_metodo_trat','feldenkrais_metodo_trat','isometrico_fortalecimiento_trat','isocarga_fortalecimiento_trat','nocarga_fortalecimiento_trat','bozu_fortalecimiento_trat','theraband_fortalecimiento_trat','reduccion_trat','rolido_marcha_trat','sentado_marcha_trat','arrastre_marcha_trat','puntos_marcha_trat','rodillas_marcha_trat','bipedo_marcha_trat','descarga_marcha_trat','equilibrio_marcha_trat','coordinacion_marcha_trat','disocion_marcha_trat','consulta_id','metodo','fortale','reduccion','tratamiento'
+    ];
 
 }
