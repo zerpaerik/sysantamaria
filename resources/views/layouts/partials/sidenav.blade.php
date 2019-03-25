c1cf9b4578518e2a9246d438c2d193e51f70f9e1
erik <zerpaerik@gmail.com>
2019-03-25 09:05:13 -0430

    menus

--- resources/views/layouts/partials/sidenav.blade.php
+++ resources/views/layouts/partials/sidenav.blade.php
@@ -699,14 +699,107 @@
 
 <li class="dropdown">
   <a href="#" class="dropdown-toggle">
-    <i class="fa fa-inbox"></i>
-    <span class="hidden-xs">Archivos</span>
+    <i class="fa fa-refresh"></i>
+    <span class="hidden-xs">Admisión</span>
   </a>
   <ul class="dropdown-menu">
-    
+   <li>
+      <a href="#" class="dropdown-toggle"><i class="fa fa-tasks"></i> Pacientes/Clientes</a>
+        <ul class="dropdown-menu">
+
+          <li>
+            <a href="{{route('pacientes.create')}}"><i class="fa fa-list-alt"></i> Registro de pacientes</a>
+          </li>
+
+
+          <li>
+            <a href="{{route('consultas.inicio')}}"><i class="fa fa-list-alt"></i> Programación de citas</a>
+          </li>
+
+
+          <li>
+            <a href="{{route('proximacita.index')}}"><i class="fa fa-list-alt"></i> Control de citas/Evaluaciones</a>
+          </li>
+
+
+        </ul>      
+    </li>
     <li>
-      <a href="{{route('pacientes.indexr')}}"><i class="fa fa-users"></i> Pacientes/clientes</a>
-    </li>    
+      <a href="#" class="dropdown-toggle"><i class="fa fa-reply"></i> Movimientos de Caja</a>
+        <ul class="dropdown-menu">
+
+          <li>
+            <a href="{{route('generics.router')}}"><i class="fa fa-plus-square-o"></i> Registro Ingresos/Gastos</a>
+          </li>
+
+           <li>
+          <a href="{{route('movimientos.index')}}"><i class="fa fa-random"></i> Registros del Dia</a>
+          </li>
+
+
+          <li>
+            <a href="{{route('cierre.index')}}"><i class="fa fa-plus-square-o"></i> Detalle Ingresos/Gastos</a>
+          </li>
+
+          <li>
+          <a href="{{route('gastos.index')}}"><i class="fa fa-random"></i> Relación de Gastos</a>
+          </li>
+
+          <li>
+            <a href="#" class="dropdown-toggle"><i class="fa fa-reply"></i> Consolidado Ingresos/Gastos</a>
+            <ul class="dropdown-menu"> 
+              <li>
+                <a href="{{route('solicitar.consolidado')}}"><i class="fa fa-plus-square-o"></i> Atención Diaria Detallado</a>
+              </li>
+              <li>
+                <a href="{{route('solicitar.diario')}}"><i class="fa fa-plus-square-o"></i> Atención Diaria Consolidado</a>
+              </li>
+            </ul>
+          </li>
+
+          <li>
+            <a href="#" class="dropdown-toggle"><i class="fa fa-reply"></i> Cobros</a>
+            <ul class="dropdown-menu"> 
+              <li>
+                <a href="{{route('cuentasporcobrar.index')}}"><i class="fa fa-plus-square-o"></i> Cuentas por Cobrar</a>
+              </li>
+              <li>
+                <a href="{{route('historialcobros.index')}}"><i class="fa fa-plus-square-o"></i> Historial de Cobros</a>
+              </li>
+            </ul>
+          </li>
+        </ul>      
+    </li>
+  </ul>
+</li>
+
+<li class="dropdown">
+  <a href="#" class="dropdown-toggle">
+    <i class="fa fa-inbox"></i>
+    <span class="hidden-xs">Asistencial</span>
+  </a>
+  <ul class="dropdown-menu">
+     <li>
+      <a href="{{route('historias.index')}}"><i class="fa fa-circle-o"></i> Historia Clínica</a>
+    </li> 
+     <li>
+      <a href="{{route('fichast.index')}}"><i class="fa fa-circle-o"></i> Fichas Terapeuticas</a>
+    </li> 
+    <li>
+      <a href="{{route('prenatal.index')}}"><i class="fa fa-users"></i> Ficha de Evaluación</a>
+    </li>
+      <li>
+        <a href="{{route('prenatal.create')}}"><i class="fa fa-list-alt"></i> Registrar Evaluación</a>
+      </li>
+      <li>
+        <a href="{{route('prenatal.index')}}"><i class="fa fa-search"></i> Buscar Evaluación</a>
+      </li>
+       <li>
+        <a href="{{route('consultas.inicio')}}"><i class="fa fa-plus-circle"></i> Listar Consultas</a>
+      </li>
+      <li>
+        <a href="{{route('service.index')}}"><i class="fa fa-plus-circle"></i> Mostrar Programación</a>
+      </li> 
   </ul>
 </li>
 
@@ -716,13 +809,11 @@
     <span class="hidden-xs">Existencias</span>
   </a>
   <ul class="dropdown-menu">
-   <li>
-      <a href="#" class="dropdown-toggle"><i class="fa fa-tasks"></i> Productos</a>
-        <ul class="dropdown-menu">
-           <li>
-            <a href="{{route('productos.index2')}}"><i class="fa fa-list-alt"></i> Almacen Local</a>
-          </li>
-        </ul>      
+    <li>
+      <a href="{{route('productos.index')}}"><i class="fa fa-list-alt"></i> Almacen Central</a>
+    </li>
+    <li>
+      <a href="{{route('productos.index2')}}"><i class="fa fa-list-alt"></i> Almacen Local</a>
     </li>
     <li>
       <a href="#" class="dropdown-toggle"><i class="fa fa-reply"></i> Requerimientos</a>
@@ -730,111 +821,17 @@
           <li>
             <a href="{{route('requerimientos.index')}}"><i class="fa fa-plus-square-o"></i> Pedido Productos</a>
           </li>
-        </ul>      
-    </li>
-    <li>
-      <a href="#" class="dropdown-toggle"><i class="fa fa-reply"></i> Salida de productos</a>
-        <ul class="dropdown-menu">
           <li>
-            <a href="{{route('productos.out')}}"><i class="fa fa-plus-square-o"></i> Ventas</a>
+            <a href="{{route('requerimientos.index2')}}"><i class="fa fa-plus-square-o"></i> Transferir Productos</a>
           </li>
-
-     
+          <li>
+            <a href="{{route('requerimientos.index3')}}"><i class="fa fa-plus-square-o"></i> Transferencia de Productos</a>
+          </li>
         </ul>      
     </li>
-  
-  <li>
-        <a href="{{route('ventas.index')}}"><i class="fa fa-plus-circle"></i> Listado de Ventas</a>
-      </li>
-
   </ul>
 </li>
 
-  <li class="dropdown">
-    <a href="#" class="dropdown-toggle">
-      <i class="fa fa-unsorted"></i>
-      <span class="hidden-xs">Consultas</span>
-    </a>
-    <ul class="dropdown-menu">
-
-      <li>
-        <a href="{{route('consultas.inicio')}}"><i class="fa fa-plus-circle"></i> Listar Consultas</a>
-      </li>  
-   
-         <li>
-        <a href="{{route('historias.index')}}"><i class="fa fa-plus-circle"></i> Ver Historias</a>
-      </li>  
-      <li>
-        <a href="{{route('consultas.index')}}"><i class="fa fa-plus-circle"></i> Mostrar Consultas</a>
-      </li>  
-         
-       <li>
-        <a href="{{route('proximacita.index')}}"><i class="fa fa-plus-circle"></i> Control de citas/Evaluaciones</a>
-      </li>                
-    </ul>
-  </li>
-  <li class="dropdown">
-    <a href="#" class="dropdown-toggle">
-      <i class="fa fa-unsorted"></i>
-      <span class="hidden-xs">Programaciones</span>
-    </a>
-    <ul class="dropdown-menu">
-      <li>
-        <a href="{{route('service.create')}}"><i class="fa fa-plus-circle"></i> Programar Servicio</a>
-      </li>
-      <li>
-        <a href="{{route('service.inicio')}}"><i class="fa fa-plus-circle"></i> Listado de Programacion</a>
-      </li> 
-      <li>
-        <a href="{{route('service.index')}}"><i class="fa fa-plus-circle"></i> Mostrar Programación</a>
-      </li>                  
-    </ul>
-  </li>
-
-    <li class="dropdown">
-    <a href="#" class="dropdown-toggle">
-      <i class="fa fa-unsorted"></i>
-      <span class="hidden-xs">Movimientos</span>
-    </a>
-    <ul class="dropdown-menu">
-      <li>
-        <a href="{{route('atenciones.index')}}"><i class="fa fa-plus-circle"></i> Ingreso de Atenciones</a>
-      </li>
-      <li>
-        <a href="{{route('gastos.index')}}"><i class="fa fa-random"></i> Relación de Gastos</a>
-      </li>
-      <li>
-        <a href="{{route('ingresos.index')}}"><i class="fa fa-money"></i> Otros Ingresos</a>
-      </li>
-      <li>
-        <a href="{{route('cuentasporcobrar.index')}}"><i class="fa fa-list-alt"></i> Cuentas por Cobrar</a>
-      </li>
-    <li>
-        <a href="{{route('historialcobros.index')}}"><i class="fa fa-list-alt"></i> Historial de Cobros</a>
-      </li>
-  
-    </ul>
-  </li>
-
-
-   <li class="dropdown">
-    <a href="#" class="dropdown-toggle">
-      <i class="fa fa-copy"></i>
-      <span class="hidden-xs">Reportes</span>
-    </a>
-    <ul class="dropdown-menu">
-        <li>
-        <a href="{{route('cierre.index')}}"><i class="fa fa-file-o"></i> Cierre de Caja</a>
-      </li> 
-      <li>
-        <a href="reporte-solicitar_diario"><i class="fa fa-file-o"></i> Atención Diaria Consolidado</a>
-      </li>
-       <li>
-        <a href="reporte-solicitar_consolidado"><i class="fa fa-file-o"></i> Atención Diaria Detallado</a>
-      </li>
-     
-    </ul>
-  </li>
 
 
    <li class="dropdown">
@@ -856,27 +853,34 @@
 @if(\Auth::user()->role_id == 7)
 
 <li class="dropdown">
-    <a href="#" class="dropdown-toggle">
-      <i class="fa fa-unsorted"></i>
-      <span class="hidden-xs">Consultas</span>
-    </a>
-    <ul class="dropdown-menu">
-        <li>
-        <a href="{{route('consultas.inicio')}}"><i class="fa fa-plus-circle"></i> Listar Consultas</a>
-      </li>  
-   
-         <li>
-        <a href="{{route('historias.index')}}"><i class="fa fa-plus-circle"></i> Ver Historias</a>
-      </li>  
+  <a href="#" class="dropdown-toggle">
+    <i class="fa fa-inbox"></i>
+    <span class="hidden-xs">Asistencial</span>
+  </a>
+  <ul class="dropdown-menu">
+     <li>
+      <a href="{{route('historias.index')}}"><i class="fa fa-circle-o"></i> Historia Clínica</a>
+    </li> 
+     <li>
+      <a href="{{route('fichast.index')}}"><i class="fa fa-circle-o"></i> Fichas Terapeuticas</a>
+    </li> 
+    <li>
+      <a href="{{route('prenatal.index')}}"><i class="fa fa-users"></i> Ficha de Evaluación</a>
+    </li>
       <li>
-        <a href="{{route('consultas.index')}}"><i class="fa fa-plus-circle"></i> Mostrar Consultas</a>
-      </li>  
-         
+        <a href="{{route('prenatal.create')}}"><i class="fa fa-list-alt"></i> Registrar Evaluación</a>
+      </li>
+      <li>
+        <a href="{{route('prenatal.index')}}"><i class="fa fa-search"></i> Buscar Evaluación</a>
+      </li>
        <li>
-        <a href="{{route('proximacita.index')}}"><i class="fa fa-plus-circle"></i> Control de citas/Evaluaciones</a>
-      </li>                 
-    </ul>
-  </li>
+        <a href="{{route('consultas.inicio')}}"><i class="fa fa-plus-circle"></i> Listar Consultas</a>
+      </li>
+      <li>
+        <a href="{{route('service.index')}}"><i class="fa fa-plus-circle"></i> Mostrar Programación</a>
+      </li> 
+  </ul>
+</li>
     <li class="dropdown">
     <a href="#" class="dropdown-toggle">
       <i class="fa fa-copy"></i>
@@ -891,37 +895,37 @@
       </li>
     </ul>
   </li>
+
   <li class="dropdown">
-    <a href="#" class="dropdown-toggle">
-      <i class="fa fa-copy"></i>
-      <span class="hidden-xs"> Control de Evaluaciones</span>
-    </a>
-    <ul class="dropdown-menu">
-      <li>
-        <a href="{{route('prenatal.create')}}"><i class="fa fa-list-alt"></i> Registrar Evaluación</a>
-      </li>
-      <li>
-        <a href="{{route('prenatal.index')}}"><i class="fa fa-search"></i> Buscar Evaluación</a>
-      </li>
-    </ul>
-  </li>
-  <li class="dropdown">
-    <a href="#" class="dropdown-toggle">
-      <i class="fa fa-unsorted"></i>
-      <span class="hidden-xs">Programaciones</span>
-    </a>
-    <ul class="dropdown-menu">
-         <li>
-        <a href="{{route('service.inicio')}}"><i class="fa fa-plus-circle"></i> Listado de Programacion</a>
-      </li> 
-      <li>
-        <a href="{{route('service.create')}}"><i class="fa fa-plus-circle"></i> Programar Servicio</a>
-      </li> 
-      <li>
-        <a href="{{route('service.index')}}"><i class="fa fa-plus-circle"></i> Mostrar Programación</a>
-      </li>                  
-    </ul>
-  </li>
+  <a href="#" class="dropdown-toggle">
+    <i class="fa fa-refresh"></i>
+    <span class="hidden-xs">Admisión</span>
+  </a>
+  <ul class="dropdown-menu">
+   <li>
+      <a href="#" class="dropdown-toggle"><i class="fa fa-tasks"></i> Pacientes/Clientes</a>
+        <ul class="dropdown-menu">
+
+          <li>
+            <a href="{{route('pacientes.create')}}"><i class="fa fa-list-alt"></i> Registro de pacientes</a>
+          </li>
+
+
+          <li>
+            <a href="{{route('consultas.inicio')}}"><i class="fa fa-list-alt"></i> Programación de citas</a>
+          </li>
+
+
+          <li>
+            <a href="{{route('proximacita.index')}}"><i class="fa fa-list-alt"></i> Control de citas/Evaluaciones</a>
+          </li>
+
+
+        </ul>      
+    </li>
+  
+  </ul>
+</li>
 
 
 
